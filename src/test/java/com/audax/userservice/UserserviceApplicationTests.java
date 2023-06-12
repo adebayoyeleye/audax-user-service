9a77ccbbadc41953db70aa2942510c0b27075789
Adebayo Oyeleye <adebayo.oyeleye@gmail.com>
2023-06-12 14:04:43 -0400

    setup: initial commit

---
+++ src/test/java/com/audax/userservice/UserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audax.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
